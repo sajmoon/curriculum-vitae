32d4a7e58e3b2895d5f6c3f98f3b2312c3786de7
Simon Strom <simon.strom@gmail.com>
2015-09-17 13:10:36 +0200

    merge all expereinces to one list with types

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import Experiences from './Experiences.jsx'
 import Header from './Header.jsx';
+import _ from 'lodash';
 
 export default React.createClass({
   propTypes: {
-    experiences: React.PropTypes.object.isRequired,
+    experiences: React.PropTypes.array.isRequired,
     details: React.PropTypes.object.isRequired
+  },
+  getWorkExperience: function() {
   },
   render() {
     let {experiences, details} = this.props;
 
+    let work = _.filter(experiences, (exp) => {
+      return exp.type === 'WORK';
+    });
+
+    let other = _.filter(experiences, (exp) => {
+      return exp.type === 'OTHER';
+    });
+
+    let commisions = _.filter(experiences, (exp) => {
+      return exp.type === 'COMMISSION';
+    });
+
+    let education = _.filter(experiences, (exp) => {
+      return exp.type === 'EDUCATION';
+    });
+
     return (
       <div>
         <Header details={details} />
-        <Experiences type="Work" experiences={experiences.work}/>
-        <Experiences type="Education" experiences={experiences.education}/>
-        <Experiences type="Commissions of Trust" experiences={experiences.commissions}/>
-        <Experiences type="Other" experiences={experiences.other}/>
+        <Experiences type="Work" experiences={work}/>
+        <Experiences type="Other" experiences={other}/>
+        <Experiences type="Commission of Trsut" experiences={commisions}/>
+        <Experiences type="Education" experiences={education}/>
       </div>
     )
   }
--- src/index.jsx
+++ src/index.jsx
@@ -2,179 +2,198 @@
 var React = require('react');
 import CV from './components/Cv.jsx';
 
-const experiences = {
-  work: [
-    {
-      from: 'Dec 2014',
-      to: 'Sept 2015',
-      title: 'Software Developer',
-      url: 'http://www.dewire.com/',
-      subtitle: 'Full time',
-      at: 'Dewire',
-      remarks: [
-        'Software developer with focus on Java/JavaEE',
-        'Backend work',
-        'AngularJs'
-      ]
-    },
-    {
-      from: '2013',
-      to: '2014',
-      title: 'Software Developer',
-      url: 'http://www.elva.se/',
-      subtitle: '',
-      at: 'Elva',
-      remarks: [
-        'Developer',
-        'Java and JavaScript',
-        'Internal projects'
-      ]
-    },
-    {
-      at: 'Hi3G (Tre)',
-      url: 'https://tre.se/',
-      from: '2012',
-      to: '2013',
-      title: 'Software Developer',
-      subtitle: '',
-      remarks: [
-        'Small group of students, planing and executing a project.',
-        'Working with Java, Ruby and Angular'
-      ]
-    },
-    {
-      at: 'Transitor AB',
-      from: '2010',
-      to: '2011',
-      title: 'Software developer',
-      url: 'http://www.comers.se/',
-      subtitle: '',
-      remarks: ['Worked with Java, creating a webapplication for bookings']
-    },
-    {
-      at: 'Student Consulting',
-      url: 'https://www.studentconsulting.com/',
-      from: '2007',
-      to: '2008',
-      title: 'Reception duty',
-      remarks: ['Reception', 'Handyman'],
-      subtitle: ''
-    },
-    {
-      at: 'Procter & Gamble',
-      url: 'http://www.pg.com/',
-      from: '2008',
-      to: '2010',
-      title: 'Several positions',
-      remarks: ['Masterdata', 'Demand planer Duracel (summer intern)'],
-      subtitle: ''
-    },
-    {
-      at: 'Fastighets AB Brostaden',
-      from: '2006',
-      to: '2007',
-      url: 'http://www.brostaden.se/',
-      title: 'Landlord',
-      subtitle: 'Summer Internship',
-      remarks: ['Summer job working with customers', 'Cleaning, painting etc']
-    }
-  ],
-  education: [
-    {
-      at: 'Royal Institute of Technology',
-      url: 'www.kth.se',
-      from: '2009',
-      title: 'Computer Science',
-      subtitle: 'Msc',
-      remarks: ['Machine Learning']
-    }
-  ],
-  commissions: [
-    {
-      at: 'THS',
-      url: 'http://ths.kth.se/',
-      from: '2013',
-      to: '2013',
-      title: 'Event Coordinator',
-      subtitle: 'Centralamottagningsgrupp',
-      remarks: ['Organize events for new students at KTH']
-    },
-    {
-      at: 'SSCO',
-      url: 'http://www.ssco.se/en',
-      from: '2013',
-      to: '2013',
-      title: 'Event Coordinator',
-      subtitle: 'Valborgsfamiljen',
-      remarks: ['Organize a pub crawl for around 1000 students in Stockholm']
-    },
-    {
-      at: 'Datasektionens Studieresa (Studs)',
-      url: 'http://studieresan.se/',
-      from: '2013',
-      to: '2013',
-      title: 'Salesteam manager',
-      subtitle: '',
-      remarks: ['Responsible for all sales', 'Managed the team']
-    },
-    {
-      at: 'Datasektionens Klubbmästeri, DKM',
-      url: 'http://dkm.io/',
-      from: '2009',
-      to: '2013',
-      title: 'Klubbmästare & Barmästare',
-      subtitle: '',
-      remarks: ['Organiza all sorts of activities for students.', 'Lots of fun', 'Leader of a group of 14 people']
-    },
-    {
-      at: 'Dartasektionens Mottagning',
-      url: 'http://datasektionen.se/sektionen/mottagningen/',
-      from: '2010',
-      to: '2011',
-      title: '',
-      subtitle: '',
-      remarks: ['Reception of new students at KTH', 'Make them feel at home'],
-    },
-    {
-      at: 'Quarnevalen',
-      from: '2014',
-      to: '2014',
-      title: 'Tältchef',
-      subtitle: '',
-      remarks: ['Organzie a huge event for students in Stockholm', 'Parade through town']
-    },
-    {
-      at: 'THS',
-      url: 'http://www.armada.nu/',
-      from: '2010',
-      to: '2011',
-      title: 'Armada',
-      subtitle: 'Företagsvärd',
-      remarks: ['A fare for students to find jobs', 'Helped Skype and Epsilon during the days.', 'Active for 2 years']
-    },
-    {
-      at: 'Datasektionen',
-      title: 'D-Dagen',
-      subtitle: 'Företagsvärd and directorate',
-      url: 'http://datasektionen.se/naringsliv/d-dagen',
-      from: '2009',
-      to: '2013',
-      remarks: ['Work fare for students interested in ComputerScience.', 'Helped organize it for 4 years']
-    }
-  ],
-  other: [
-    {
-      at: 'Försvarsmakten',
-      from: '2006',
-      to: '2007',
-      title: 'Stridsfordonsplutonbefäl',
-      subtitle: 'Värnplikt',
-      url: 'http://www.forsvarsmakten.se/',
-      remarks: ['Military Service in Sweden', 'Leader of a pluton of around 60 people.']
+const experiences = [
+  {
+    from: 'Dec 2014',
+    type: 'WORK',
+    to: 'Sept 2015',
+    title: 'Software Developer',
+    url: 'http://www.dewire.com/',
+    subtitle: 'Full time',
+    at: 'Dewire',
+    remarks: [
+      'Software developer with focus on Java/JavaEE',
+      'Backend work',
+      'AngularJs'
+    ]
+  },
+  {
+    from: '2013',
+    type: 'WORK',
+    to: '2014',
+    title: 'Software Developer',
+    url: 'http://www.elva.se/',
+    subtitle: '',
+    at: 'Elva',
+    remarks: [
+      'Developer',
+      'Java and JavaScript',
+      'Internal projects'
+    ]
+  },
+  {
+    at: 'Hi3G (Tre)',
+    type: 'WORK',
+    url: 'https://tre.se/',
+    from: '2012',
+    to: '2013',
+    title: 'Software Developer',
+    subtitle: '',
+    remarks: [
+      'Small group of students, planing and executing a project.',
+      'Working with Java, Ruby and Angular'
+    ]
+  },
+  {
+    at: 'Transitor AB',
+    type: 'WORK',
+    from: '2010',
+    to: '2011',
+    title: 'Software developer',
+    url: 'http://www.comers.se/',
+    subtitle: '',
+    remarks: ['Worked with Java, creating a webapplication for bookings']
+  },
+  {
+    at: 'Student Consulting',
+    type: 'WORK',
+    url: 'https://www.studentconsulting.com/',
+    from: '2007',
+    to: '2008',
+    title: 'Reception duty',
+    remarks: ['Reception', 'Handyman'],
+    subtitle: ''
+  },
+  {
+    at: 'Procter & Gamble',
+    url: 'http://www.pg.com/',
+    from: '2008',
+    to: '2010',
+    title: 'Several positions',
+    remarks: ['Masterdata', 'Demand planer Duracel (summer intern)'],
+    subtitle: '',
+    type: 'WORK'
+  },
+  {
+    at: 'Fastighets AB Brostaden',
+    from: '2006',
+    type: 'WORK',
+    to: '2007',
+    url: 'http://www.brostaden.se/',
+    title: 'Landlord',
+    subtitle: 'Summer Internship',
+    remarks: ['Summer job working with customers', 'Cleaning, painting etc']
+  },
+  {
+    at: 'Royal Institute of Technology',
+    type: 'EDUCATION',
+    url: 'www.kth.se',
+    from: '2009',
+    title: 'Computer Science',
+    subtitle: 'Msc',
+    remarks: ['Machine Learning']
+  },
+  {
+    at: 'Samskolan Saltsjöbaden',
+    type: 'EDUCATION',
+    url: 'www.ibo.org',
+    from: '2003',
+    to: '2006',
+    title: 'International baccalaureate',
+    subtitle: '',
+    remarks: ['Studied Math, Computer Science English on higher level.']
+  },
+  {
+    at: 'THS',
+    url: 'http://ths.kth.se/',
+    from: '2013',
+    type: 'COMMISSION',
+    to: '2013',
+    title: 'Event Coordinator',
+    subtitle: 'Centralamottagningsgrupp',
+    remarks: ['Organize events for new students at KTH']
+  },
+  {
+    at: 'SSCO',
+    url: 'http://www.ssco.se/en',
+    from: '2013',
+    type: 'COMMISSION',
+    to: '2013',
+    title: 'Event Coordinator',
+    subtitle: 'Valborgsfamiljen',
+    remarks: ['Organize a pub crawl for around 1000 students in Stockholm']
+  },
+  {
+    at: 'Datasektionens Studieresa (Studs)',
+    url: 'http://studieresan.se/',
+    from: '2013',
+    type: 'COMMISSION',
+    to: '2013',
+    title: 'Salesteam manager',
+    subtitle: '',
+    remarks: ['Responsible for all sales', 'Managed the team']
+  },
+  {
+    at: 'Datasektionens Klubbmästeri, DKM',
+    url: 'http://dkm.io/',
+    from: '2009',
+    type: 'COMMISSION',
+    to: '2013',
+    title: 'Klubbmästare & Barmästare',
+    subtitle: '',
+    remarks: ['Organiza all sorts of activities for students.', 'Lots of fun', 'Leader of a group of 14 people']
+  },
+  {
+    at: 'Dartasektionens Mottagning',
+    url: 'http://datasektionen.se/sektionen/mottagningen/',
+    from: '2010',
+    to: '2011',
+    type: 'COMMISSION',
+    title: '',
+    subtitle: '',
+    remarks: ['Reception of new students at KTH', 'Make them feel at home'],
+  },
+  {
+    at: 'Quarnevalen',
+    from: '2014',
+    to: '2014',
+    title: 'Tältchef',
+    type: 'COMMISSION',
+    subtitle: '',
+    remarks: ['Organzie a huge event for students in Stockholm', 'Parade through town']
+  },
+  {
+    at: 'THS',
+    url: 'http://www.armada.nu/',
+    type: 'COMMISSION',
+    from: '2010',
+    to: '2011',
+    title: 'Armada',
+    subtitle: 'Företagsvärd',
+    remarks: ['A fare for students to find jobs', 'Helped Skype and Epsilon during the days.', 'Active for 2 years']
+  },
+  {
+    at: 'Datasektionen',
+    title: 'D-Dagen',
+    type: 'COMMISSION',
+    subtitle: 'Företagsvärd and directorate',
+    url: 'http://datasektionen.se/naringsliv/d-dagen',
+    from: '2009',
+    to: '2013',
+    remarks: ['Work fare for students interested in ComputerScience.', 'Helped organize it for 4 years']
+  },
+  {
+    at: 'Försvarsmakten',
+    from: '2006',
+    type: 'OTHER',
+    to: '2007',
+    title: 'Stridsfordonsplutonbefäl',
+    subtitle: 'Värnplikt',
+    url: 'http://www.forsvarsmakten.se/',
+    remarks: ['Military Service in Sweden', 'Leader of a pluton of around 60 people.']
 
-    }
-  ]
-};
+  }
+];
 
 const details = {
   name: "Simon Ström",

2557dc511a8350215596a0b9e7f642a96310fc77
Simon Strom <simon.strom@gmail.com>
2016-11-28 23:01:47 +0100

    Style the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,18 @@
 import React, { PropTypes } from 'react'
 
+const style = {
+  margin: '20px',
+  border: '0',
+  cursor: 'pointer',
+  background: 'rgb(168, 186, 240)',
+  color: '#2574A9',
+  fontSize: '18px',
+  padding: '10px'
+}
+
 function Button(props) {
   return (
-    <button type="button" onClick={props.onClick}>
+    <button style={style} type="button" onClick={props.onClick}>
       { props.children }
     </button>
   )
